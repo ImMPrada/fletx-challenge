03907ae9a4b0584b822b151d2d4c88f28166416e
immprada <im.mprada@gmail.com>
2025-04-18 09:31:00 -0500

    add container template, for centering all in the middle

--- frontend/src/templates/center-midle-container/index.tsx
+++ frontend/src/templates/center-midle-container/index.tsx
@@ -2,7 +2,7 @@ import { CenterMidleContainerProps } from "./types";
 
 const CenterMidleContainer = ({children}: CenterMidleContainerProps) => {
     return (
-        <div className="flex items-center justify-center h-screen w-screen">
+        <div className="flex items-center justify-center h-screen w-screen font-sans bg-white-light">
             {children}
         </div>
     )
