e91ca70a598874ff7abda9b7b7207246718d4377
immprada <im.mprada@gmail.com>
2025-04-18 09:30:33 -0500

    add Button UI component

---
+++ frontend/src/components/ui/button/types.ts
@@ -0,0 +1,5 @@
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string;
+  type: "button" | "submit" | "reset";
+  variant: "primary" | "secondary";
+}
