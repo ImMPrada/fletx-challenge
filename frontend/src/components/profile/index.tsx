3386065ad7eddabf9b9c2c614c54eb8403baddd9
immprada <im.mprada@gmail.com>
2025-04-20 06:15:49 -0500

    add profile component

---
+++ frontend/src/components/profile/index.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useMe } from "../../hooks/use-me";
+import Loading from "../loading";
+
+const Profile = () => {
+  const { user, isLoading, fetchUser } = useMe();
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>{user?.email}</p>
+    </div>
+  );
+};
+
+export default Profile;
