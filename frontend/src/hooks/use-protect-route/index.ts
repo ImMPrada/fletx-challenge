4159cf63e0244e4ad08799b47da958ec10fb2aae
immprada <im.mprada@gmail.com>
2025-04-20 06:16:26 -0500

    add hook to check if there are a session, if not, redirects to login

---
+++ frontend/src/hooks/use-protect-route/index.ts
@@ -0,0 +1,27 @@
+import { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FlashContext } from '../../contexts/flash-context';
+import { AuthContext } from '../../contexts/auth-context';
+
+export const useProtectRoute = () => {
+  const navigate = useNavigate();
+  const { setFlash } = useContext(FlashContext);
+  const { state } = useContext(AuthContext);
+
+  // Verificar si el contexto de autenticación indica que el usuario está autenticado
+  const verifyAuthentication = (): boolean => {
+    return state.isAuthenticated;
+  };
+
+  // Verificar la autenticación al montar el componente
+  useEffect(() => {
+    const isAuthenticated = verifyAuthentication();
+    
+    if (!isAuthenticated) {
+      setFlash('Debe iniciar sesión para acceder a esta ruta', 'warning');
+      navigate('/login');
+    }
+  }, []);
+
+  return;
+}; 
